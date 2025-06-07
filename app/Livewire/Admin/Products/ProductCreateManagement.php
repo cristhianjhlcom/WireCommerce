8a018240f2254923dcfc6b6ce39ad9042a01de20
Cristhian <cristhianjhl.web@gmail.com>
2025-06-06 23:22:02 -0500

    wip

--- app/Livewire/Admin/Products/ProductCreateManagement.php
+++ app/Livewire/Admin/Products/ProductCreateManagement.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace App\Livewire\Admin\Products;
 
+use App\Enums\CurrenciesCodeEnum;
 use App\Enums\PermissionsEnum;
+use App\Enums\ProductsStatusEnum;
 use App\Models\Category;
+use App\Models\Color;
 use App\Models\Product;
+use App\Models\Size;
 use App\Models\Tag;
 use Exception;
 use Flux\Flux;
@@ -15,20 +19,109 @@ use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Money\Currencies;
 
 // TODO: Usar un parent component AdminComponent.
 #[Layout('layouts.admin')]
 #[Title('Product Management')]
 final class ProductCreateManagement extends Component
 {
-    #[Validate]
+    #[Validate('required|string|max:255')]
     public string $name = '';
 
+    #[Validate('required|unique:products,slug')]
     public string $slug = '';
 
-    public string $description = '';
+    #[Validate('nullable|string|max:255')]
+    public ?string $description = null;
+
+    #[Validate('required|exists:categories,id')]
+    public $category_id = null;
+
+    #[Validate('nullable|string|max:255')]
+    public ?string $seo_title = null;
+
+    #[Validate('nullable|string|max:255')]
+    public ?string $seo_description = null;
+
+    public string $variant_sku = '';
+    public string $variant_color_id = '';
+    public string $variant_size_id = '';
+    public string $variant_price = '';
+    public string $variant_sale_price = '';
+    public CurrenciesCodeEnum $variant_currency_code = CurrenciesCodeEnum::PEN;
+    public ProductsStatusEnum $variant_statu = ProductsStatusEnum::ACTIVE;
+    public ?string $variant_image = null;
+    public array $variants = [
+        [
+            'sku' => 'AF001',
+            'status' => ProductsStatusEnum::ACTIVE,
+            'price' => 9999,
+            'sale_price' => 5999,
+            'color_id' => 1,
+            'size_id' => 1,
+            'currency_code' => CurrenciesCodeEnum::PEN,
+            'image' => null,
+        ],
+        [
+            'sku' => 'AF002',
+            'status' => ProductsStatusEnum::ACTIVE,
+            'price' => 9999,
+            'sale_price' => 0,
+            'color_id' => 1,
+            'size_id' => 1,
+            'currency_code' => CurrenciesCodeEnum::PEN,
+            'image' => null,
+        ],
+    ];
 
     public ?string $image = null;
+
+    public mixed $colors;
+    public mixed $sizes;
+    public mixed $categories;
+
+    protected function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'category_id' => 'required|exists:categories,id',
+            'seo_title' => 'nullable|string|max:255',
+            'seo_description' => 'nullable|string',
+            'variants' => 'required|array|min:1',
+            'variants.*.sku' => 'required|string|unique:product_variants,sku',
+            'variants.*.price' => 'required|integer|min:1',
+            'variants.*.sale_price' => 'nullable|integer|min:1',
+            'variants.*.color_id' => 'nullable|exists:colors,id',
+            'variants.*.size_id' => 'nullable|exists:sizes,id',
+            'variants.*.image' => 'nullable|file|max:1024',
+        ];
+    }
+
+    public function addVariant()
+    {
+        $this->variants[] = [
+            'sku' => $this->variant_sku,
+            'price' => $this->variant_price,
+            'sale_price' => (int) $this->variant_sale_price,
+            'color_id' => (int) $this->variant_color_id,
+            'size_id' => $this->variant_size_id,
+            'currency_code' => $this->variant_currency_code,
+            'status' => $this->variant_statu,
+            'image' => $this->variant_image,
+        ];
+
+        Flux::modal('add-variant')->close();
+    }
+
+    public function mount()
+    {
+        $this->colors = Color::all();
+        $this->sizes = Size::all();
+        $this->categories = Category::all();
+    }
+
 
     public function updating($property, $value)
     {
@@ -74,18 +167,9 @@ final class ProductCreateManagement extends Component
     public function render()
     {
         return view('livewire.admin.products.create', [
-            'categories' => Category::all(),
             'tags' => Tag::all(),
+            'variants' => $this->variants,
+            'status' => ProductsStatusEnum::cases(),
         ]);
-    }
-
-    protected function rules(): array
-    {
-        return [
-            'name' => 'required|string|max:255',
-            'slug' => 'required|min:3|unique:categories,slug',
-            'description' => 'required|string|max:255',
-            'image' => 'nullable|image|max:2048',
-        ];
     }
 }
--- resources/views/livewire/admin/products/create.blade.php
+++ resources/views/livewire/admin/products/create.blade.php
@@ -10,11 +10,11 @@
       </flux:button>
     </div>
   </div>
-  <flux:separator />
+
   <form class="space-y-4" wire:submit.prevent="save">
-    <div class="flex items-start justify-between gap-x-4">
+    <div class="flex flex-col gap-x-4 md:flex-row md:items-start md:justify-between">
       {{-- First Section --}}
-      <div class="space-y-4 md:w-2/3">
+      <div class="w-full space-y-4 md:w-2/3">
         <flux:card class="space-y-4">
           <flux:field>
             <flux:input placeholder="{{ __('Gold Ring') }}" wire:model.live='name' />
@@ -30,7 +30,6 @@
             wire:model="description"
           />
         </flux:card>
-
         <flux:card class="space-y-4">
           <flux:field>
             <flux:label>{{ __('Media') }}</flux:label>
@@ -39,20 +38,161 @@
             </flux:description>
             <flux:input
               accept="image/*"
+              multiple
               type="file"
-              wire:model.lazy="image"
+              wire:model.lazy="images"
             />
             <flux:errorname="image" />
           </flux:field>
         </flux:card>
-
         <flux:card class="space-y-4">
-          <h1>Variants</h1>
+          <div class="flex items-center justify-between gap-x-4">
+            <div>
+              <flux:heading>{{ __('Variants Combinations') }}</flux:heading>
+              <flux:text class="mt-2">
+                {{ __('Select the combinations you want to sell and set the price for each one.') }}
+              </flux:text>
+            </div>
+            <flux:modal.trigger name="add-variant">
+              <flux:button icon:trailing="plus">
+                {{ __('Add New Variant') }}
+              </flux:button>
+            </flux:modal.trigger>
+            <flux:modal class="md:w-[500px]" name="add-variant">
+              <div class="space-y-4">
+                <div>
+                  <flux:heading size="lg">{{ __('Add New Variant') }}</flux:heading>
+                  <flux:text class="mt-2">
+                    {{ __('Select the combinations you want to sell and set the price for each one.') }}
+                  </flux:text>
+                </div>
+                <div class="grid grid-cols-2 gap-x-4 gap-y-6">
+                  <flux:input
+                    label="SKU"
+                    placeholder="Unique SKU"
+                    wire:model="variant_sku"
+                  />
+                  <flux:field>
+                    <flux:label>
+                      {{ __('Status') }}
+                    </flux:label>
+                    <flux:select placeholder="{{ __('Select Status') }}" wire:model="variant_statu">
+                      @foreach ($status as $statu)
+                        <flux:select.option value="{{ $statu->value }}" wire:key="{{ $statu->value }}">
+                          {{ $statu->label() }}
+                        </flux:select.option>
+                      @endforeach
+                    </flux:select>
+                    <flux:error name="variant_statu" />
+                  </flux:field>
+                </div>
+
+                <flux:field>
+                  <flux:label>
+                    {{ __('Price') }}
+                  </flux:label>
+                  <flux:input
+                    placeholder="S/. 99.99"
+                    type="text"
+                    wire:model="variant_price"
+                  />
+                  <flux:error name="variant_price" />
+                </flux:field>
+                <flux:field>
+                  <flux:label>
+                    {{ __('Sale Price') }}
+                  </flux:label>
+                  <flux:input
+                    placeholder="S/. 59.99"
+                    type="text"
+                    wire:model="variant_sale_price"
+                  />
+                  <flux:error name="variant_sale_price" />
+                  <flux:description>
+                    Deja este campo vacío para no incluir en el precio con descuento.
+                  </flux:description>
+                </flux:field>
+                <div class="grid grid-cols-2 gap-x-4 gap-y-6">
+                  <flux:field>
+                    <flux:label>
+                      {{ __('Color') }}
+                    </flux:label>
+                    <flux:select placeholder="{{ __('Select Color') }}" wire:model="color_id">
+                      @foreach ($colors as $color)
+                        <flux:select.option value="{{ $color->id }}">
+                          {{ $color->name }}
+                        </flux:select.option>
+                      @endforeach
+                    </flux:select>
+                    <flux:error name="color_id" />
+                  </flux:field>
+                  <flux:field>
+                    <flux:label>
+                      {{ __('Size') }}
+                    </flux:label>
+                    <flux:select placeholder="{{ __('Select Size') }}" wire:model="size_id">
+                      @foreach ($sizes as $size)
+                        <flux:select.option value="{{ $size->id }}">
+                          {{ $size->name }}
+                        </flux:select.option>
+                      @endforeach
+                    </flux:select>
+                    <flux:error name="size_id" />
+                  </flux:field>
+                </div>
+                <flux:input
+                  label="Variant Image"
+                  type="file"
+                  wire:model="variant_image"
+                />
+                <div class="flex">
+                  <flux:spacer />
+                  <flux:button
+                    type="button"
+                    variant="primary"
+                    wire:click="addVariant"
+                  >
+                    {{ __('Create Variant') }}
+                  </flux:button>
+                </div>
+              </div>
+            </flux:modal>
+          </div>
+          <flux:table>
+            <flux:table.columns>
+              <flux:table.column>{{ __('Variant') }}</flux:table.column>
+              <flux:table.column>{{ __('Status') }}</flux:table.column>
+              <flux:table.column>{{ __('Price') }}</flux:table.column>
+              <flux:table.column>{{ __('Sale Price') }}</flux:table.column>
+              <flux:table.column>{{ __('Actions') }}</flux:table.column>
+            </flux:table.columns>
+            <flux:table.rows>
+              @forelse ($variants as $variant)
+                <flux:table.row>
+                  <flux:table.cell>{{ $variant['sku'] }}</flux:table.cell>
+                  <flux:table.cell>Jul 29, 10:45 AM</flux:table.cell>
+                  <flux:table.cell>
+                    <flux:badge
+                      color="green"
+                      inset="top bottom"
+                      size="sm"
+                    >Paid</flux:badge>
+                  </flux:table.cell>
+                  <flux:table.cell variant="strong">$49.00</flux:table.cell>
+                  <flux:table.cell variant="strong">$49.00</flux:table.cell>
+                </flux:table.row>
+              @empty
+                <flux:table.row>
+                  <flux:table.cell>{{ __('No Records') }}</flux:table.cell>
+                </flux:table.row>
+              @endforelse
+            </flux:table.rows>
+          </flux:table>
         </flux:card>
       </div>
 
       {{-- Second Section --}}
-      <div class="space-y-4 md:w-1/3">
+      <div class="w-full space-y-4 md:w-1/3">
         <flux:card class="space-y-4">
           <flux:field>
             <flux:label>{{ __('Status') }}</flux:label>
@@ -94,6 +234,23 @@
             </flux:select>
             <flux:error name="tags" />
           </flux:field>
+        </flux:card>
+
+        <flux:card class="space-y-4">
+          <flux:input
+            label="{{ __('Seo Title') }}"
+            name="seo_title"
+            placeholder="consectetur adipiscing elit 📦"
+            type="text"
+            wire:model="seo_title"
+          />
+          <flux:textarea
+            label="{{ __('Seo Description') }}"
+            name="seo_description"
+            placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue sapien eget arcu tristique venenatis..."
+            rows="2"
+            wire:model="seo_description"
+          />
         </flux:card>
       </div>
     </div>
